28772b00f8ab5fe583681f618eda11069dadd539
thanhtai2102 <46438729+thanhtai2102@users.noreply.github.com>
2019-07-23 14:24:00 +0700

    fix prod connect postgres

--- src/models/index.js
+++ src/models/index.js
@@ -8,27 +8,39 @@ let dbConfig = {
   DATABASE_PASSWORD:'abcd1234',
 }
 
+let sequelize = null
 
 if (process.env.NODE_ENV === "production") {
   dbConfig = {
     DATABASE: process.env.DATABASE_URL || 'postgres://xwlmrkzbcrnoxp:bd2ca6cf649338bb6c9f2cc482c1d5902785825bc1733682c61107515ce6e072@ec2-50-19-231-222.compute-1.amazonaws.com:5432/d970hatls108fr',
-    DATABASE_USER:'tttgalaxy.awshosting@gmail.com',
-    DATABASE_PASSWORD:'PhanHang2102',
+    // DATABASE_USER:'tttgalaxy.awshosting@gmail.com',
+    // DATABASE_PASSWORD:'PhanHang2102',
   }
+  sequelize = new Sequelize(process.env.DATABASE_URL || dbConfig.DATABASE, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging:  true //false
+  })
+} else {
+  sequelize = new Sequelize(
+    process.env.DATABASE || dbConfig.DATABASE,
+    process.env.DATABASE_USER || dbConfig.DATABASE_USER,
+    process.env.DATABASE_PASSWORD || dbConfig.DATABASE_PASSWORD,
+    {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+          ssl: true
+      }
+    },
+  );
 }
 
-const sequelize = new Sequelize(
-  process.env.DATABASE || dbConfig.DATABASE,
-  process.env.DATABASE_USER || dbConfig.DATABASE_USER,
-  process.env.DATABASE_PASSWORD || dbConfig.DATABASE_PASSWORD,
-  {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-        ssl: true
-    }
-  },
-);
+
+
+
 
 const models = {
   User: sequelize.import('./user.model')

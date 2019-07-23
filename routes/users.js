f3758adb10fcc6e8c38981e5aa6d4a8835813df7
thanhtai2102 <46438729+thanhtai2102@users.noreply.github.com>
2019-07-23 10:21:47 +0700

    add basic api

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
+const { models, sequelize } = require('./../src/models');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.post('/login', async function(req, res, next) {
+  let data = req.body;
+    let user = await findUserByEmail(data.email);
+    if (user) {
+      if (user.password === data.password) {
+        let token = jwt.sign({
+          email: user.email,
+        }, secret, { expiresIn : 60*60*24 });
+        res.json({
+          message: 'Login success!',
+          token: token
+        })
+      } else {
+        res.json({
+          message: 'wrong username or password!'
+        })
+      }
+    } else {
+      res.json({
+        message: 'wrong username or password!'
+      })
+    }
+});
+
+router.post('/register', async function (req, res) {
+  let data = req.body;
+  let user = await findUserByEmail(data.email);
+  if (!user) {
+    let user = await models.User.createUser(data)
+    res.json({
+      message: 'created user',
+      data: user
+    })
+  } else {
+    res.json({
+      message: 'user is existed'
+    })
+  }
+})
+
+
+
+async function findUserByEmail (email) {
+  return await models.User.findByLogin(email)
+}
 
 module.exports = router;
---
+++ src/models/index.js
@@ -0,0 +1,33 @@
+"use strict"
+
+const Sequelize = require('sequelize');
+
+let dbConfig = {
+  DATABASE:'tttgalaxy',
+  DATABASE_USER:'postgres',
+  DATABASE_PASSWORD:'abcd1234',
+}
+
+const sequelize = new Sequelize(
+  process.env.DATABASE || dbConfig.DATABASE,
+  process.env.DATABASE_USER || dbConfig.DATABASE_USER,
+  process.env.DATABASE_PASSWORD || dbConfig.DATABASE_PASSWORD,
+  {
+    dialect: 'postgres',
+  },
+);
+
+const models = {
+  User: sequelize.import('./user.model')
+};
+
+Object.keys(models).forEach(key => {
+  if ('associate' in models[key]) {
+    models[key].associate(models);
+  }
+});
+
+module.exports = {
+  models,
+  sequelize
+};

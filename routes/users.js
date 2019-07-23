e074388f51a5ab246381e39919e9ca1f92d77276
thanhtai2102 <46438729+thanhtai2102@users.noreply.github.com>
2019-07-23 15:15:27 +0700

    add json web token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const jwt = require('jwt')
+const jwt = require('jsonwebtoken')
 
 const { models, sequelize } = require('./../src/models');
 
